e61f6b762ed910f684a065502caee0c8eff36515
cel71 <celestialc71@libero.it>
2021-07-06 18:05:26 +0200

    vuejs-search-jobs es1

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const app = new Vue (
+    {
+        el: "#app",
+        data: {
+            jobs: [
+                {
+                    id: 1,
+                    company: 'Perferendis',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 2,
+                    company: 'Perferendis',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 3,
+                    company: 'Perferendis',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 4,
+                    company: 'Perferendis',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+                {
+                    id: 5,
+                    company: 'Perferendis',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },
+            ],
+            starred: [2, 4],
+            applied: [4, 5],
+            iconaPositiva: "icona-positiva",
+            iconaNegativa: "icona-negativa",
+        },
+        methods: {
+
+        },           
+    },
+);
+
